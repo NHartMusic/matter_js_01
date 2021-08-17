8e258659c84da755aabe5f22dffbdeaa373c3f20
Nick Hart <nhartmusic@gmail.com>
2021-08-17 16:02:07 +0200

    initial project setup

---
+++ script.js
@@ -0,0 +1,13 @@
+//module aliases 
+var Engine = Matter.Engine,
+    // Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies
+
+function setup() {
+    createCanvas(600, 600)
+}
+
+function draw() {
+    background(51)
+}
